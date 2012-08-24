ee98afe141214f83a33d6e899e271df09a8ee1ff
Jakub Milkiewicz <jmilkiewicz@gmail.com>
2012-08-24 15:28:06 +0200

    adding list to enable specify multiple @SpelAssert annotations on single type

--- src/main/java/pl/softmil/validator/el/constraint/SpelAssert.java
+++ src/main/java/pl/softmil/validator/el/constraint/SpelAssert.java
@@ -1,17 +1,15 @@
 package pl.softmil.validator.el.constraint;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-import javax.validation.Constraint;
-import javax.validation.Payload;
+import java.lang.annotation.*;
+
+import javax.validation.*;
 
 import pl.softmil.validator.el.impl.SpelAssertValidator;
 
-@Target(ElementType.TYPE)
+@Target({ TYPE, ANNOTATION_TYPE })
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = SpelAssertValidator.class)
 @Documented
@@ -25,4 +23,11 @@ public @interface SpelAssert {
     Class<? extends Payload>[] payload() default {};
 
     String value();
+
+    @Target({ ANNOTATION_TYPE, TYPE })
+    @Retention(RUNTIME)
+    @Documented
+    @interface List {
+        SpelAssert[] value();
+    }
 }
