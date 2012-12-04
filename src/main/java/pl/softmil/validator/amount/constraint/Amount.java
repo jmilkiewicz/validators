91f2c194c702928a435f14f0977590939730bb13
Jakub Milkiewicz <jmilkiewicz@gmail.com>
2012-12-04 11:53:33 +0100

    organizing static imports

--- src/main/java/pl/softmil/validator/amount/constraint/Amount.java
+++ src/main/java/pl/softmil/validator/amount/constraint/Amount.java
@@ -1,10 +1,6 @@
 package pl.softmil.validator.amount.constraint;
 
-import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
-import static java.lang.annotation.ElementType.CONSTRUCTOR;
-import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Documented;
